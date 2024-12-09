a1b83c9cae91db4ed216464b8ce7b463d1a7d18e
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-12-09 17:06:33 +0700

    feat(home): add doctor and shop cells to home tab

--- lib/presentation/home/home_tab_screen.dart
+++ lib/presentation/home/home_tab_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_medicare/common_widget/category_button.dart';
+import 'package:flutter_medicare/common_widget/section_row.dart';
+import 'package:flutter_medicare/presentation/home/doctor_cell.dart';
+import 'package:flutter_medicare/presentation/home/shop_cell.dart';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
 import '../../common/color_extension.dart';
 
 class HomeTabScreen extends StatefulWidget {
@@ -45,6 +49,41 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
       // "title": "Ad 2",
       "image": "assets/img/ad_2.png",
     }
+  ];
+  List nearDoctorArr = [
+    {
+      "name": "Dr. Mainsh",
+      "degree": "BAMS, MBBS, MD, DDLT,Psychiatrist",
+      "img": "assets/img/u1.png"
+    },
+    {
+      "name": "Dr. Priyanka",
+      "degree": "BAMS, MBBS, MD, DDLT,Psychiatrist",
+      "img": "assets/img/u2.png"
+    },
+    {
+      "name": "Dr. Kunal Vaidya",
+      "degree": "BAMS, MBBS, MD, DDLT,Psychiatrist",
+      "img": "assets/img/u3.png"
+    },
+  ];
+
+  List nearShopArr = [
+    {
+      "name": "Jai Ambey Medical",
+      "address": "Surat",
+      "img": "assets/img/s1.png"
+    },
+    {
+      "name": "All Relif Medical",
+      "address": "Surat",
+      "img": "assets/img/s2.png"
+    },
+    {
+      "name": "Matru Chaya Medical",
+      "address": "Surat",
+      "img": "assets/img/s3.png"
+    },
   ];
   @override
   Widget build(BuildContext context) {
@@ -112,7 +151,49 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
                 ),
                 itemCount: adsArr.length,
               ),
-            )
+            ),
+            SectionRow(
+              title: "Doctor near you",
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 220,
+              child: ListView.separated(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return DoctorCell(
+                      obj: nearDoctorArr[index],
+                      onPressed: () {},
+                    );
+                  },
+                  separatorBuilder: (context, index) => const SizedBox(
+                        width: 20,
+                      ),
+                  itemCount: nearDoctorArr.length),
+            ),
+            SectionRow(
+              title: "Medical Shop near you",
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 220,
+              child: ListView.separated(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return ShopCell(
+                      obj: nearShopArr[index],
+                      onPressed: () {},
+                    );
+                  },
+                  separatorBuilder: (context, index) => const SizedBox(
+                        width: 20,
+                      ),
+                  itemCount: nearShopArr.length),
+            ),
           ],
         ),
       ),
